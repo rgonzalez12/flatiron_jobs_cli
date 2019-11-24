b35c95bd4c3bada22a0916fd7a703b44534cb2c2
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-24 01:47:00 +0000

    removed config directory, moved files to lib

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'greenhouse_io'
-
-require_relative "..lib/flatiron_jobs_cli_project/version"
-require_relative "..lib/flatiron_jobs_cli_project/job_post"
-require_relative "..lib/flatiron_jobs_cli_project/cli"
-require_relative "..lib/flatiron_jobs_cli_project/api"
-
