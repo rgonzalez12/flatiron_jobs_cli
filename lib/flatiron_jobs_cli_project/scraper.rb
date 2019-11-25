32bbab4f64403326dc57cba942fd8e5bc9530769
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-11-25 06:43:17 +0000

    verified api calls functionality. pushed to production

--- lib/flatiron_jobs_cli_project/scraper.rb
+++ lib/flatiron_jobs_cli_project/scraper.rb
@@ -1,7 +1,7 @@
 class Scraper
   
   def all_jobs_call
-   url = "https://boards-api.greenhouse.io/v1/boards/flatironschoolcareers/jobs"
+    url = "https://boards-api.greenhouse.io/v1/boards/flatironschoolcareers/jobs"
     response = HTTParty.get(url)
     response["jobs"].each do |job|
       absolute_url = job["absolute_url"]
@@ -12,11 +12,23 @@ class Scraper
     end
   end
   
-  def self.offices_call
-    https://boards-api.greenhouse.io/v1/boards/flatironschoolcareers/offices
+  def offices_call
+    url = "https://boards-api.greenhouse.io/v1/boards/flatironschoolcareers/offices"
+    response = HTTParty.get(url)
+    response["offices"].each do |office|
+      name = office["name"]
+      location = office["location"]
+      id = office["id"]
+    end
   end
 
-  def self.departments_call
-    https://boards-api.greenhouse.io/v1/boards/flatironschoolcareers/departments
+  def departments_call
+    url = "https://boards-api.greenhouse.io/v1/boards/flatironschoolcareers/departments"
+    response = HTTParty.get(url)
+    response["departments"].each do |department|
+      id = department["id"]
+      name = department["name"]
+    end
   end
+  
 end
