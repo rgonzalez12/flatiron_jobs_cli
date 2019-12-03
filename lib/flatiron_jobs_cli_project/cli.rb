bc8269aafa17a0b1a9e8a8e4391c17564bda1812
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-12-03 06:11:31 +0000

    adjusted cli calls and api

--- lib/flatiron_jobs_cli_project/cli.rb
+++ lib/flatiron_jobs_cli_project/cli.rb
@@ -14,36 +14,43 @@ class JobsBoard
     input = nil
     
     while input != "exit"
-      sleep(1.5)
+      sleep(0.5)
       puts "To see all the current openings, type 'list jobs'."
-      sleep(1.5)
+      sleep(0.5)
       puts "You can also close the program by typing 'exit'."
-      sleep(1.5)
+      sleep(0.5)
       puts "What would you like to do?"
 
       input = gets.strip.downcase
       
-      if input == "list jobs"
+      if input.to_i > 0
+        job_info(input)
+      elsif input == "list jobs"
         list_all
       elsif input == "exit"
         exit_message
       else
-        puts "\nThis is not a valid option."
+        puts "\nThis is not a valid option. Please choose one of the available options."
       end 
     end
   end
 
   def list_all
-    puts "Here are all of the current openings at Flatiron School. Enter the job number you would like to view."
+    puts "Here are all of the current openings at Flatiron School."
     sleep(1)
     
     JobPost.all.each.with_index(1) do |job, i|
       puts "#{i}. #{job.title}"
+      sleep(0.25)
     end
+    
+    puts "Enter the job number you wish to view."
+    sleep(2)
   end
   
-  def job_info
-    puts "#{jobs.title} - #{jobs.location} - #{jobs.departments} - #{jobs.offices} - #{jobs.content}. Copy and paste the link below to apply! #{jobs.absolute_url}"
+  def job_info(input)
+    
+    puts "#{job.title} - #{job.location} - #{job.departments} - #{job.offices} - #{job.content}. Copy and paste the link below to apply! #{job.absolute_url}"
     #lists info from the specific jobs
   end
   
